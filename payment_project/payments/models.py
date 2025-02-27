ca97546b9835ee4988a038ce4bd3eaa330e084b0
liliyakudasheva <oduvan4ik_888@mail.ru>
2025-02-27 20:34:59 +0300

    start

---
+++ payment_project/payments/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Item(models.Model):
+    CURRENCY_CHOICES = [
+        ('usd', 'USD'),
+        ('eur', 'EUR'),
+    ]
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.IntegerField()  # Цена в центах
+    currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES)
+
+    def __str__(self):
+        return self.name
